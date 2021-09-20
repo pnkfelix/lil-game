4077bf88b07c05c22f351b6c0b0f26b23c2f6b3d
Felix Klock <pnkfelix@amazon.com>
2021-09-19 23:32:20 -0400

    More fleshed out still.
    1. Add an end-game check
    2. more command error handling
    3. make the AI selection actually select the next move
    4. Debug instrumentation

--- service/src/local.rs
+++ service/src/local.rs
@@ -1,7 +1,9 @@
+use tracing::debug;
+
 use std::io::{self, BufRead, Write};
 use std::convert::TryInto;
 
-use crate::game_core::{Command, Game};
+use crate::game_core::{Command, Game, Move};
 
 mod game_core;
 mod tictactoe;
@@ -9,6 +11,8 @@ mod tictactoe;
 type TheGame = crate::tictactoe::TicTacToeGame;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+
     let stdin = io::stdin();
     let mut lines = stdin.lock().lines();
 
@@ -29,6 +33,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             // no command provided; retry.
             prompt();
             continue;
+        }
+        if line.to_lowercase() == "q" || line.to_lowercase() == "quit" {
+            // user asked to quit. Do it.
+            return Ok(());
         }
         let (cmd, slash_state) = line.split_at(1);
         if slash_state == "" {
@@ -53,9 +61,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
 
-        let c: Command = cmd.chars().next().unwrap().try_into()?;
+        debug!("line: {:?}, cmd: {:?} slash_state: {:?}", line, cmd, slash_state);
+        let c: Command = match cmd.chars().next().unwrap().try_into() {
+            Ok(c) => c,
+            Err(_) => {
+                println!("`{}` is not a valid command", cmd);
+                prompt();
+                continue;
+            }
+        };
 
         let unparsed = game.unparse();
+        debug!("c: {:?} unparsed: {:?}", c, unparsed);
+
         match c {
             Command::NewGame => {
                 game = Default::default();
@@ -67,6 +85,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     .map(|m|(m.id, m.next_state.unparse()))
                     .collect::<Vec<_>>();
 
+                let chosen_move: &Move<TheGame>;
                 'choose: loop {
                     println!("list {:?} : {:?}", unparsed, moves_unparsed);
 
@@ -94,7 +113,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                     };
 
                     'confirm: loop {
-                        println!("Move {} yields \n{}",
+                        println!("Move {} yields\n{}",
                                  num,
                                  m.next_state.render_to_text());
                         println!("Is this what you want (Y/n)?");
@@ -102,7 +121,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                             match &line.to_lowercase()[..] {
                                 "n" | "no" => continue 'choose,
                                 "" | "y" | "yes" => {
-                                    game = m.next_state.clone();
+                                    chosen_move = m;
                                     break 'choose;
                                 }
                                 _ => {
@@ -114,16 +133,33 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         }
                     }
                 }
+
+                game = end_game_check(chosen_move);
             }
             Command::RenderToText => {
                 println!("render {:?} :\n{}", unparsed, game.render_to_text());
             }
             Command::Select => {
-                println!("select {:?} : {:?}", unparsed, game_core::search(&game.moves()));
+                let next_moves = game.moves();
+                let choice = game_core::search(&next_moves);
+                println!("select {:?} : {:?}", unparsed, choice);
+                println!("AI chose\n{}", choice.next_state.render_to_text());
+
+                game = end_game_check(choice);
             }
         }
 
         prompt();
     }
     Ok(())
+}
+
+fn end_game_check<B: Game>(the_move: &Move<B>) -> B {
+    if let Some(victors) = &the_move.end_game {
+        println!("game over! Victory goes to {:?}", victors);
+        println!("starting new game.");
+        Default::default()
+    } else {
+        the_move.next_state.clone()
+    }
 }
